23771ce6428ddbff65dfe52ec4ead08032a4180b
Ian Bailey <IanKulin@kulin.com.au>
2024-07-01 14:49:40 +0800

    initial

---
+++ image-fetch.py
@@ -0,0 +1,18 @@
+# download an image from the internet
+# (https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png)
+# then save it to the './data' directory in the current working directory
+
+import requests
+import os
+
+url = ('https://www.google.com/images/branding/googlelogo/2x/'
+       'googlelogo_color_272x92dp.png')
+filepath = './data/google_logo.png'
+
+# create the directory if it doesn't exist
+os.makedirs(os.path.dirname(filepath), exist_ok=True)
+
+response = requests.get(url)
+with open(filepath, 'wb') as f:
+    f.write(response.content)
+    print('Image downloaded successfully.')
